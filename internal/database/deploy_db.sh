098b6a67511181b5f2dee4c554f238b39e1454d5
vulkan0n <miltonrojer@gmail.com>
2023-09-19 19:48:46 -0300

    Add deploy database bash script

---
+++ internal/database/deploy_db.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+#Must be run as sudo
+
+#Copy the scripts files to the postgres directory
+cp -r ../database/ ~postgres/
+cd ~postgres/
+
+#Run scripts with psql: db_superbchat.sql  account.sql  session.sql  superchat.sql
+echo "Creating 'superbchat' database"
+sudo -u postgres -H -- psql -U postgres -d postgres -f database/db_superbchat.sql
+echo "Creating 'account' table"
+sudo -u postgres -H -- psql -U postgres -d superbchat -f database/account.sql
+echo "Creating 'session' table"
+sudo -u postgres -H -- psql -U postgres -d superbchat -f database/session.sql
+echo "Creating 'superchat' table"
+sudo -u postgres -H -- psql -U postgres -d superbchat -f database/superchat.sql
+#Create 'web' user
+echo "Creating 'web' user"
+sudo -u postgres -H -- psql -U postgres -d superbchat -c "CREATE USER web WITH PASSWORD 'pass';"
+sudo -u postgres -H -- psql -U postgres -d superbchat -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO web;"
+sudo -u postgres -H -- psql -U postgres -d superbchat -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO web;"
+
+#Remove scripts from postgres directory
+rm -r -f database/
+
